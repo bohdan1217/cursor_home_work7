4598e3087d044561280f79175e4d1ee23302e173
Богдан Гордійчук <bogdangordijcuk@Bogdans-MacBook-Pro.local>
2019-03-29 11:07:06 +0200

    add forEach

--- script.js
+++ script.js
@@ -10,6 +10,18 @@
         75: "K",
     };
 
+    let parent = document.querySelector('.piano');
+    Object.keys(keys).forEach(function(key, id) {
+        let newChild = '<div id="key-'+ keys[key] +'" class="piano-key"><p class="key">' + keys[key] + '</p></div>';
+        parent.insertAdjacentHTML('beforeend', newChild);
+    });
+
+    let parentAudio =  document.querySelector('.audio');
+    Object.keys(keys).forEach(function(key, id) {
+        let newChildAudio = '<audio id="sound-'+ keys[key] +'"><source src="samples/'+ ++id +'.mp3" type="audio/mpeg"></audio>';
+        parentAudio.insertAdjacentHTML('beforeend', newChildAudio);
+    });
+
     function sound(eventKey) {
         let ring = document.getElementById("sound-" + eventKey);
         ring.currentTime = 0;
@@ -25,25 +37,14 @@
     window.addEventListener("keydown", function (event) {
         let charCode = (typeof event.which == "number") ? event.which : event.keyCode;
         let eventKey = (keys[charCode]);
-
-        if(!eventKey) {
-            return;
-        }
-        else {
-            sound(eventKey);
-        }
+        eventKey ? sound(eventKey) : null
     });
 
     window.addEventListener("mousedown", function (event) {
         let eventClassName = event.target.className == 'key' || event.target.className == 'piano-key';
-        if (eventClassName == false) {
-            return;
-        } else {
-            let eventKey =  event.target.textContent;
-            eventKey = eventKey.replace(/\s/g, '');
-
-            sound(eventKey);
-        }
+        let eventKey =  event.target.textContent;
+        eventKey = eventKey.replace(/\s/g, '');
+        eventClassName ? sound(eventKey) : null
     });
 
 })();
